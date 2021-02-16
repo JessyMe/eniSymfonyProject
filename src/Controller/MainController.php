b680c5e591f4b5cf3737d13ff2ea05a84f892b94
MathieuChenoun <chenoun.mathieu@gmail.com>
2021-02-16 09:20:14 +0100

    form en cours

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
+use App\Form\ListSortieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +14,20 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-    public function List(): Response
+    public function List(Request $request): Response
     {
         $sortie = new Sortie();
         $sortieForm = $this->createForm(ListSortieType::class,$sortie);
         if($sortieForm->isSubmitted() && $sortieForm->isValid()){
-            return $this->render('Page/home.html.twig',["sortieList" => $sorties] );
+            $sortieForm->handleRequest($request);
+
+
+
+
+
+
+
+            return $this->render('Page/home.html.twig');
         }else{
             $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
             $sorties = $sortieRepository->findAll();
