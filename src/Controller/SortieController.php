2725a6f7a8fa9631b2cd91288e363b7c2242bf7e
JessyMe <jessy.mercer@gmail.com>
2021-02-16 15:12:35 +0100

    sortieType

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,8 +25,23 @@ class SortieController extends AbstractController
     /**
      * @Route ("/add", name="add")
      */
-    public function add()
+    public function add(EntityManagerInterface $em, Request $request)
     {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
 
+        $sortieForm->handleRequest($request);
+        if ($sortieForm->isValid() && $sortieForm->isSubmitted()){
+            $em->persist($sortie);
+            $em->flush();
+
+            $this->addFlash('success', 'Nouvelle sortie publiée');
+            return $this->redirectToRoute("main_home");
+        }
+
+        return $this->render("sortie/add.html.twig", [
+            'sortieForm'=>$sortieForm->createView()
+        ]);
     }
+
 }
