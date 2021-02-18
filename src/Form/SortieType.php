aea008b4cc722dea5e9aea85ae63629ca53d97f7
JessyMe <jessy.mercer@gmail.com>
2021-02-18 17:03:34 +0100

    Creer Sortie formulaire suite2

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -3,14 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Campus;
-use App\Entity\Lieu;
 use App\Entity\Sortie;
-use App\Entity\Ville;
-use phpDocumentor\Reflection\Type;
-use phpDocumentor\Reflection\Types\False_;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
