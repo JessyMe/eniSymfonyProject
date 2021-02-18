716b70bc33a21e71fe2d518b6b0e6ff41b57c1d6
sly09 <sylvain.garcia72@sfr.fr>
2021-02-18 17:30:13 +0100

    Mon profil

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -18,6 +19,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegisterType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -59,6 +61,14 @@ class RegisterType extends AbstractType
                 'attr' => ['class' =>'form-control']
             ])
         ;
+        $builder->get('photo')->addModelTransformer(new CallBackTransformer(
+            function($photo) {
+                return null;
+            },
+            function($photo) {
+                return $photo;
+            }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
