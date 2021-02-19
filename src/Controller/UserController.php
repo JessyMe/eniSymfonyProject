8aeb40158e5f11521ed812260192aa3447131545
sly09 <sylvain.garcia72@sfr.fr>
2021-02-19 14:33:37 +0100

    Fin première itération!

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +22,7 @@ class UserController extends AbstractController
     {
         $user = new User();
         $registerForm = $this->createForm(RegisterType::class, $user);
-        $user->setDateCreated(new \DateTime());
+        $user->setDateCreated(new DateTime());
         $user->setRoles(['ROLE_USER']);
 
 
@@ -46,21 +47,26 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/profil/{id}", name="user_profil", methods={"GET"})
+     * @Route("/profil", name="user_profil")
      */
-    public function modifierProfil(Request $request, EntityManagerInterface $em, $id): Response
+    public function modifierProfil(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
     {
 
 
-        $user = $em->getRepository(User::class)->find($id);
+        $user = $em->getRepository(User::class)->find($this->getUser());
         $registerForm = $this->createForm(RegisterType::class, $user);
         $registerForm->handleRequest($request);
 
         if ($registerForm->isSubmitted() && $registerForm->isValid())
         {
-
+            //hash password
+            $hashed = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashed);
 
             $em->flush();
+
+            $this->addFlash('success', 'Votre profil a bien été mis à jour !');
+            return $this->redirectToRoute('main_home');
         }
 
         return $this->render('user/register.html.twig', [
