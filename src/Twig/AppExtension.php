10c5b980b197907138c1b8b5fd09c4300c170d9b
MathieuChenoun <chenoun.mathieu@gmail.com>
2021-02-22 14:41:38 +0100

    MAJ home

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,15 +4,28 @@
 namespace App\Twig;
 
 
-use App\Entity\Inscription;
-use phpDocumentor\Reflection\Types\Integer;
+use DateTime;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
 
-    public function getInscriptions($sortieId): Integer
+    /**
+     * @inheritDoc
+     */
+    public function getFunctions(): array
     {
+        return [
+            new TwigFunction('convert', [$this, 'convert'])
+        ];
+    }
+    public function convert($dt)
+    {
+       $dateDepartTimeStamp = strtotime($dt);
+       dump($dt);
+       dump($dateDepartTimeStamp);
 
+       return $datePlusOneMonth = date("d/m/y", strtotime('+ 1 month',$dateDepartTimeStamp));
     }
 }
