7ae5798d71b59677e99f81c3e2d989a279fcad4c
MathieuChenoun <chenoun.mathieu@gmail.com>
2021-02-25 15:30:05 +0100

    Version Mobile de l'application

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,14 +4,14 @@ namespace App\Controller;
 
 //use App\Entity\Etat;
 use App\Entity\Inscription;
+use App\Entity\User;
 use App\Service\ListFormSortie;
 use App\Entity\Campus;
 use App\Entity\Sortie;
 use App\Form\ListSortieType;
-//use Doctrine\ORM\EntityManagerInterface;
-//use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -19,46 +19,102 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-    public function List(Request $request)
+    public function List(Request $request, PropertyAccessorInterface $pa)
     {
-       // $this->setEtatSortie();
-        //Liste Campus pour menu déroulant
-        $campusRepository = $this->getDoctrine()->getRepository(Campus::class);
-        $campus = $campusRepository->findAll();
-        //création du formulaire
-        $ListFormSortie = new ListFormSortie();
-        $sortieForm = $this->createForm(ListSortieType::class,$ListFormSortie);
-        $sortieForm->handleRequest($request);
+//----------- CHECK Type de device ----------------------------
+    $headers = $request->headers->all();
+    $head = $pa->getValue($headers, '[user-agent]');
+    $userAgent = $pa->getValue($head, '[0]');
+    dump($request);
+    dump($headers);
+    dump($head);
+    dump($userAgent);
 
-        //Liste des inscriptions pour affichage
-        $InscriptionRepository = $this->getDoctrine()->getRepository(Inscription::class);
-        $inscriptions = $InscriptionRepository->findAll();
+    $needle = 'Mobile';
+    $mobileOrNot = strpos($userAgent, $needle);
+    dump($mobileOrNot);
 
-        if($sortieForm->isSubmitted()){
-            $userLog = $this->getUser();
+    //--------------- Desktop --------------------------------
+        if ($mobileOrNot === false)
+        {
+            // $this->setEtatSortie();
+            //Liste Campus pour menu déroulant
+            $campusRepository = $this->getDoctrine()->getRepository(Campus::class);
+            $campus = $campusRepository->findAll();
+            //création du formulaire
+            $ListFormSortie = new ListFormSortie();
+            $sortieForm = $this->createForm(ListSortieType::class,$ListFormSortie);
+            $sortieForm->handleRequest($request);
+
+            //Liste des inscriptions pour affichage
+            $InscriptionRepository = $this->getDoctrine()->getRepository(Inscription::class);
+            $inscriptions = $InscriptionRepository->findAll();
+
+            if($sortieForm->isSubmitted()){
+                $userLog = $this->getUser();
+                $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+                $sorties = $sortieRepository->findByFormFilter($ListFormSortie, $userLog);
+                dump($sorties);
+                return $this->render('Page/home.html.twig',
+                    ["user"=>$userLog,
+                        "inscriptions"=>$inscriptions,
+                        "sorties"=>$sorties,
+                        "campus"=>$campus,
+                        "sortieForm"=>$sortieForm->createView()]);
+            }else{
+                $userLog = $this->getUser();
+                $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+                $sorties = $sortieRepository->findAll();
+                return $this->render('Page/home.html.twig',
+                    ["user"=>$userLog,
+                        "inscriptions"=>$inscriptions,
+                        "sorties"=>$sorties,
+                        "campus"=>$campus,
+                        "sortieForm"=>$sortieForm->createView()]);
+            }
+        //------------- Redirect to mobile -----------------
+        }else if($mobileOrNot != false)
+            {
+                return $this->redirectToRoute('home_mobile');
+            }
+        }
+
+    /**
+     * @Route("/m", name="home_mobile")
+     */
+        public function  ListMobile ()
+        {
+            $userLog = $this->getUser()->getUsername();
+            $userRepository = $this->getDoctrine()->getRepository(User::class);
+            $user = $userRepository->findBy(['email'=>$userLog]);
+            dump($user);
+
+            //list des sorties pour le campus de l'user connecté
             $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
-            $sorties = $sortieRepository->findByFormFilter($ListFormSortie, $userLog);
-            dump($sorties);
-            return $this->render('Page/home.html.twig',
+            $sortieCampusUser = $sortieRepository->findBy(['campus' => $user[0]->getCampus()]);
+            dump($sortieCampusUser);
+            //Liste des inscriptions pour affichage
+            $InscriptionRepository = $this->getDoctrine()->getRepository(Inscription::class);
+            $inscriptions = $InscriptionRepository->findAll();
+
+
+            return $this->render('Page/homeMobile.html.twig',
                 ["user"=>$userLog,
                     "inscriptions"=>$inscriptions,
-                    "sorties"=>$sorties,
-                    "campus"=>$campus,
-                    "sortieForm"=>$sortieForm->createView()]);
-        }else{
-            $userLog = $this->getUser();
-            $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
-            $sorties = $sortieRepository->findAll();
-            return $this->render('Page/home.html.twig',
-                ["user"=>$userLog,
-                    "inscriptions"=>$inscriptions,
-                    "sorties"=>$sorties,
-                    "campus"=>$campus,
-                    "sortieForm"=>$sortieForm->createView()]);
+                    "sorties"=>$sortieCampusUser
+                    ]);
+
+
+
+
+
         }
 
 
-        }
+
+
+
+
 
 //        public function setEtatSortie()
 //        {
